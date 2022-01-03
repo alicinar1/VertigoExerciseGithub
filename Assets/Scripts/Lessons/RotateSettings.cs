f208fc423b457dbe7ae8cd999c9964ebab3c2780
alicinar1 <alicinar1@users.noreply.github.com>
2022-01-03 22:05:24 +0300

    FirstLessonEnd

---
+++ Assets/Scripts/Lessons/RotateSettings.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lessons
+{
+    [CreateAssetMenu(fileName = "RotateSettings", menuName = "Lessons/Lesson1/Rotate Settings")]
+    public class RotateSettings : ScriptableObject
+    {
+        [SerializeField] private float _speed = 1;
+
+        public float Speed { get { return _speed; } }
+    }
+}
